8e34599fc39cec4706223c3fb9e19e6197c11ac4
SannaJuoperi <sanna.juoperi@centria.fi>
2025-02-23 23:20:13 +0200

    omasivu luotu

---
+++ ReactApp/omasivu/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Section from "./components/Section.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Section id="home" title="Home" text="This is the home section. Scroll down to explore more!"/>
+        <Section id="about" title="About" text="This is the about section. Learn more about this page here."/>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
